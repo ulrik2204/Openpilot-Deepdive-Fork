83b95ef59c7e0fecae0afa9e532042095a5fedb6
Ulrik Røsby <ulrik@rosby.no>
2024-02-18 16:12:40 +0100

    fix: slurm scripts and more test

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
+import time
+
+from tqdm import tqdm
+
+
 def main():
     print("Hello World!")
     with open("test.txt", "w") as f:
         f.write("Hello World! from file")
+    for i in tqdm(range(10)):
+        print("index", i)
+        time.sleep(2)
 
 
 if __name__ == "__main__":
