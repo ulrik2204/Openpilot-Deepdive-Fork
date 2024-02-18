9cd67a4a474bcafc5c52a0c215a1b534275d017e
Ulrik Røsby <ulrik@rosby.no>
2024-02-18 22:45:12 +0100

    fix: more slurm tests

--- test.py
+++ test.py
@@ -7,8 +7,11 @@ def main():
     print("Hello World!")
     with open("test.txt", "w") as f:
         f.write("Hello World! from file")
-    for i in tqdm(range(10)):
-        print("index", i)
+    for _ in tqdm(range(10)):
+        time.sleep(2)
+    for j in range(10):
+        if j % 2 == 0:
+            print("index", j)
         time.sleep(2)
 
 
